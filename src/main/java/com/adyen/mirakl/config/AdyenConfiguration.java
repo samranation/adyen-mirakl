e472498126bf598208e603cf623731567d38403b
daniel.gander <daniel.gander@e2x.co.uk>
2018-02-16 14:25:13 +0000

    Use setEnvironment on Client instead of Config

--- src/main/java/com/adyen/mirakl/config/AdyenConfiguration.java
+++ src/main/java/com/adyen/mirakl/config/AdyenConfiguration.java
@@ -1,12 +1,13 @@
 package com.adyen.mirakl.config;
 
-import com.adyen.Client;
-import com.adyen.Config;
-import com.adyen.enums.Environment;
-import com.adyen.service.Notification;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.adyen.Client;
+import com.adyen.Config;
+import com.adyen.enums.Environment;
+import com.adyen.service.Account;
+import com.adyen.service.Notification;
 
 @Configuration
 @ConfigurationProperties(prefix = "adyenConfig", ignoreUnknownFields = false)
@@ -16,7 +17,6 @@ public class AdyenConfiguration {
     private String password;
     private Environment environment;
     private String appName;
-    private String endpoint;
 
     public String getUserName() {
         return userName;
@@ -50,32 +50,29 @@ public class AdyenConfiguration {
         this.appName = appName;
     }
 
-    public String getEndpoint() {
-        return endpoint;
-    }
-
-    public void setEndpoint(final String endpoint) {
-        this.endpoint = endpoint;
-    }
-
     @Bean
     public Config adyenConfig(){
         final Config config = new Config();
         config.setUsername(userName);
         config.setPassword(password);
-        config.setEnvironment(environment);
         config.setApplicationName(appName);
-        config.setMarketPayEndpoint(endpoint);
         return config;
     }
 
     @Bean
     public Client adyenClient(){
-        return new Client(adyenConfig());
+        Client client = new Client(adyenConfig());
+        client.setEnvironment(environment);
+        return client;
     }
 
     @Bean
     public Notification adyenNotification(){
         return new Notification(adyenClient());
+    }
+
+    @Bean
+    public Account createAccountService() {
+        return new Account(adyenClient());
     }
 }
