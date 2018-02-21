6932abb8a184382b7e92358656c7abe401e2a1e6
daniel.gander <daniel.gander@e2x.co.uk>
2018-02-21 11:32:54 +0000

    [DG] ADY-38 review comments

--- src/main/java/com/adyen/mirakl/listeners/AdyenNotificationListener.java
+++ src/main/java/com/adyen/mirakl/listeners/AdyenNotificationListener.java
@@ -1,6 +1,8 @@
 package com.adyen.mirakl.listeners;
 
 import com.adyen.mirakl.events.AdyenNotifcationEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -8,10 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class AdyenNotificationListener {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Async
     @EventListener
     public void handleContextRefresh(AdyenNotifcationEvent event) {
-        System.out.println(event.getDbId());
+        log.info(String.format("Received notification DB id: [%d]", event.getDbId()));
     }
 
 }
