9f2ec4e6aedcff7a65dd62f37c195e286ff5302e
pandemicGander <dannygonline@gmail.com>
2018-02-22 17:57:32 +0000

    [DG] ADY-53 As an Operator I would like to create an Account Holder in Adyen Marketpay when a Business Seller supplies Shareholder Data

--- src/main/java/com/adyen/mirakl/service/ShopService.java
+++ src/main/java/com/adyen/mirakl/service/ShopService.java
@@ -1,30 +1,12 @@
 package com.adyen.mirakl.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import javax.annotation.Resource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import com.adyen.mirakl.startup.MiraklStartupValidator;
 import com.adyen.model.Name;
-import com.adyen.model.marketpay.AccountHolderDetails;
-import com.adyen.model.marketpay.BankAccountDetail;
-import com.adyen.model.marketpay.CreateAccountHolderRequest;
+import com.adyen.model.marketpay.*;
 import com.adyen.model.marketpay.CreateAccountHolderRequest.LegalEntityEnum;
-import com.adyen.model.marketpay.CreateAccountHolderResponse;
-import com.adyen.model.marketpay.GetAccountHolderRequest;
-import com.adyen.model.marketpay.GetAccountHolderResponse;
-import com.adyen.model.marketpay.IndividualDetails;
-import com.adyen.model.marketpay.UpdateAccountHolderRequest;
-import com.adyen.model.marketpay.UpdateAccountHolderResponse;
 import com.adyen.service.Account;
 import com.adyen.service.exception.ApiException;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.mirakl.client.mmp.domain.additionalfield.MiraklAdditionalFieldType;
 import com.mirakl.client.mmp.domain.common.MiraklAdditionalFieldValue;
@@ -35,6 +17,17 @@ import com.mirakl.client.mmp.domain.shop.MiraklShops;
 import com.mirakl.client.mmp.domain.shop.bank.MiraklIbanBankAccountInformation;
 import com.mirakl.client.mmp.operator.core.MiraklMarketplacePlatformOperatorApiClient;
 import com.mirakl.client.mmp.request.shop.MiraklGetShopsRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @Transactional
@@ -113,7 +106,10 @@ public class ShopService {
         if (LegalEntityEnum.INDIVIDUAL.equals(legalEntity)) {
             IndividualDetails individualDetails = createIndividualDetailsFromShop(shop);
             accountHolderDetails.setIndividualDetails(individualDetails);
-        } else {
+        } else if (LegalEntityEnum.BUSINESS.equals(legalEntity)) {
+            BusinessDetails businessDetails = createBusinessDetailsFromShop(shop);
+            accountHolderDetails.setBusinessDetails(businessDetails);
+        }else{
             throw new RuntimeException(legalEntity.toString() + " not supported");
         }
 
@@ -145,6 +141,13 @@ public class ShopService {
         return Optional.of(shop.getContactInformation()).orElseThrow(() -> new RuntimeException("Contact information not found"));
     }
 
+
+    private BusinessDetails createBusinessDetailsFromShop(final MiraklShop shop) {
+        BusinessDetails businessDetails = new BusinessDetails();
+        businessDetails.setShareholders(extractUbos(shop));
+        return businessDetails;
+    }
+
     private IndividualDetails createIndividualDetailsFromShop(MiraklShop shop) {
         IndividualDetails individualDetails = new IndividualDetails();
 
@@ -153,11 +156,7 @@ public class ShopService {
         Name name = new Name();
         name.setFirstName(contactInformation.getFirstname());
         name.setLastName(contactInformation.getLastname());
-        if (CIVILITY_TO_GENDER.containsKey(contactInformation.getCivility())) {
-            name.setGender(CIVILITY_TO_GENDER.get(contactInformation.getCivility()));
-        } else {
-            name.setGender(Name.GenderEnum.UNKNOWN);
-        }
+        name.setGender(CIVILITY_TO_GENDER.getOrDefault(contactInformation.getCivility(), Name.GenderEnum.UNKNOWN));
         individualDetails.setName(name);
         return individualDetails;
     }
@@ -236,6 +235,50 @@ public class ShopService {
         accountHolderDetails.setBankAccountDetails(bankAccountDetails);
 
         return bankAccountDetail;
+    }
+
+    private List<ShareholderContact> extractUbos(final MiraklShop shop) {
+        Map<String, String> extractedKeysFromMirakl = shop.getAdditionalFieldValues().stream()
+            .filter(MiraklAdditionalFieldValue.MiraklAbstractAdditionalFieldWithSingleValue.class::isInstance)
+            .map(MiraklAdditionalFieldValue.MiraklAbstractAdditionalFieldWithSingleValue.class::cast)
+            .collect(Collectors.toMap(MiraklAdditionalFieldValue.MiraklAbstractAdditionalFieldWithSingleValue::getValue, MiraklAdditionalFieldValue::getCode));
+
+        ImmutableList.Builder<ShareholderContact> builder = ImmutableList.builder();
+        generateKeys().forEach((i, keys) -> {
+            String firstName = extractedKeysFromMirakl.getOrDefault(keys.get(0), "");
+            String lastName = extractedKeysFromMirakl.getOrDefault(keys.get(1), "");
+            String gender = extractedKeysFromMirakl.getOrDefault(keys.get(2), "");
+            String email = extractedKeysFromMirakl.getOrDefault(keys.get(3), "");
+
+            if(ImmutableList.of(firstName, lastName, gender, email).stream().noneMatch(StringUtils::isBlank)){
+                ShareholderContact shareholderContact = new ShareholderContact();
+                Name name = new Name();
+                name.setFirstName(firstName);
+                name.setLastName(lastName);
+                name.setGender(CIVILITY_TO_GENDER.getOrDefault(gender, Name.GenderEnum.UNKNOWN));
+                shareholderContact.setName(name);
+                shareholderContact.setEmail(email);
+                builder.add(shareholderContact);
+            }
+        });
+        return builder.build();
+    }
+
+    private Map<Integer, List<String>> generateKeys(){
+        return IntStream.of(1, 2, 3, 4)
+            .mapToObj(i -> {
+                final Map<Integer, List<String>> grouped = new HashMap<>();
+                grouped.put(i, ImmutableList.of(
+                    "adyen-ubo" + String.valueOf(i) + "-firstname",
+                    "adyen-ubo" + String.valueOf(i) + "-lastname",
+                    "adyen-ubo" + String.valueOf(i) + "-gender",
+                    "adyen-ubo" + String.valueOf(i) + "-email"));
+                return grouped;
+            }).reduce((x, y) -> {
+                x.put(y.entrySet().iterator().next().getKey()
+                    , y.entrySet().iterator().next().getValue());
+                return x;
+            }).get();
     }
 
     /**
