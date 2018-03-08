f78e8570c6e1de221414c3bd0a4ede90887112f7
amrik.samra <amrik.samra@e2x.co.uk>
2018-03-08 11:43:19 +0000

    [AS] bug tags + MockMVC to post voucher to Connector

--- src/test/java/com/adyen/mirakl/cucumber/stepdefs/MiraklPaymentVoucherSteps.java
+++ src/test/java/com/adyen/mirakl/cucumber/stepdefs/MiraklPaymentVoucherSteps.java
@@ -1,48 +1,46 @@
 package com.adyen.mirakl.cucumber.stepdefs;
 
-import com.adyen.mirakl.cucumber.stepdefs.helpers.stepshelper.StepDefsHelper;
+import com.adyen.mirakl.web.rest.MiraklNotificationsResource;
+import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import cucumber.api.DataTable;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.When;
-import io.restassured.RestAssured;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.io.File;
 import java.net.URL;
 
 import static com.adyen.mirakl.cucumber.stepdefs.helpers.hooks.CucumberHooks.cucumberTable;
 import static com.adyen.mirakl.cucumber.stepdefs.helpers.hooks.CucumberHooks.rows;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class MiraklPaymentVoucherSteps extends StepDefsHelper {
+public class MiraklPaymentVoucherSteps extends StepDefs{
 
-//    @Autowired
-//    private UserResource userResource;
-//
-//    private MockMvc restUserMockMvc;
+    @Autowired
+    private MiraklNotificationsResource miraklNotificationsResource;
+
+    private MockMvc restUserMockMvc;
+
+    @Before
+    public void setup() {
+        this.restUserMockMvc = MockMvcBuilders.standaloneSetup(miraklNotificationsResource).build();
+    }
 
     @When("^a payment voucher is sent to the Connector$")
     public void aPaymentVoucherIsSentToAdyen(DataTable table) throws Exception {
         cucumberTable.put("table", table);
         String paymentVoucher = rows().get(0).get("paymentVoucher").toString();
         URL url = Resources.getResource("paymentvouchers/"+paymentVoucher);
-        RestAssured
-            .given()
-                .multiPart(new File(url.getPath()))
-            .when()
-                .post("/api/mirakl-notifications/payout")
-            .then()
-                .statusCode(200);
-//        String paymentVoucher = rows().get(0).get("paymentVoucher").toString();
-//        URL url = Resources.getResource("paymentvouchers/");
-//        restUserMockMvc = MockMvcBuilders.standaloneSetup(
-//            new FileResource(
-//                new File(url.toString()),
-//                new File(url.toString()+paymentVoucher))).build();
-//
-//        MockMultipartFile mockMultipartFile = new MockMultipartFile(paymentVoucher, paymentVoucher.getBytes());
-//
-//        MockHttpServletResponse response = restUserMockMvc.perform(fileUpload("https://adyen-requestbin.herokuapp.com/17en1211").file(mockMultipartFile))
-//            .andReturn().getResponse();
-//        int status = response.getStatus();
-//        System.out.println("\n posted!"+ status);
+        final String csvFile = Resources.toString(url, Charsets.UTF_8);
+
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", paymentVoucher, "text/plain", csvFile.getBytes());
+
+        restUserMockMvc.perform(MockMvcRequestBuilders.fileUpload("/api/mirakl-notifications/payout")
+            .file(mockMultipartFile))
+            .andExpect(status().is(200));
     }
 }
