ef36654ac83e4254003a5989f27d08e42d3de276
amrik.samra <amrik.samra@e2x.co.uk>
2018-03-10 18:35:21 +0000

    [AS] Jsoup parser for email validation

--- src/test/java/com/adyen/mirakl/cucumber/stepdefs/EmailMessagesSteps.java
+++ src/test/java/com/adyen/mirakl/cucumber/stepdefs/EmailMessagesSteps.java
@@ -6,6 +6,8 @@ import cucumber.api.java.en.Then;
 import io.restassured.RestAssured;
 import io.restassured.response.ResponseBody;
 import org.assertj.core.api.Assertions;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.util.List;
 import java.util.Map;
@@ -14,26 +16,23 @@ import static com.adyen.mirakl.cucumber.stepdefs.helpers.hooks.CucumberHooks.cuc
 
 public class EmailMessagesSteps extends StepDefsHelper {
     @Then("^an email will be sent to the seller$")
-    public void anEmailWillBeSentToTheSeller() throws Throwable {
+    public void anEmailWillBeSentToTheSeller(String message) throws Throwable {
         MiraklShop createdShop = (MiraklShop) cucumberMap.get("createdShop");
         String email = createdShop.getContactInformation().getEmail();
 
         ResponseBody responseBody = RestAssured.get(mailTrapConfiguration.mailTrapEndPoint()).thenReturn().body();
         List<Map<String, Object>> emailLists = responseBody.jsonPath().get();
 
-        // TODO: find html parser which will make it easier to find the body of the html email
-        String message = null;
+        String htmlBody = null;
         for (Map list : emailLists) {
             if (list.get("to_email").equals(email)) {
-                message = list.get("html_body").toString();
+                htmlBody = list.get("html_body").toString();
                 break;
-            } else {
-                message = "";
             }
         }
-
-        //TODO: get the actual message
-        Assertions.assertThat(message)
-            .contains("");
+        if (htmlBody != null) {
+            Document parse = Jsoup.parse(htmlBody);
+            Assertions.assertThat(parse.body().text()).contains(message);
+        }
     }
 }
