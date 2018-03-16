296d6a06ce2dcbb65f04680b34a8e477fdf5cf2e
pandemicGander <dannygonline@gmail.com>
2018-03-16 11:12:01 +0000

    [DG] ADY-18 ADY-101 composite primary key

--- src/main/java/com/adyen/mirakl/domain/ShareholderMapping.java
+++ src/main/java/com/adyen/mirakl/domain/ShareholderMapping.java
@@ -15,18 +15,17 @@ import java.util.Objects;
 @Entity
 @Table(name = "shareholder_mapping")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@IdClass(ShareHolderMappingPk.class )
 public class ShareholderMapping implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @NotNull
     @Column(name = "mirakl_shop_id", nullable = false)
     private String miraklShopId;
 
+    @Id
     @NotNull
     @Column(name = "mirakl_ubo_number", nullable = false)
     private Integer miraklUboNumber;
@@ -36,14 +35,6 @@ public class ShareholderMapping implements Serializable {
     private String adyenShareholderCode;
 
     // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getMiraklShopId() {
         return miraklShopId;
     }
@@ -93,21 +84,21 @@ public class ShareholderMapping implements Serializable {
             return false;
         }
         ShareholderMapping shareholderMapping = (ShareholderMapping) o;
-        if (shareholderMapping.getId() == null || getId() == null) {
+        if (shareholderMapping.getMiraklShopId() == null || getMiraklShopId() == null ||
+            shareholderMapping.getMiraklUboNumber() == null || getMiraklUboNumber() == null) {
             return false;
         }
-        return Objects.equals(getId(), shareholderMapping.getId());
+        return Objects.equals(getMiraklShopId()+getMiraklUboNumber(), shareholderMapping.getMiraklShopId()+shareholderMapping.getMiraklUboNumber());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(getId());
+        return Objects.hashCode(getMiraklShopId()+getMiraklUboNumber());
     }
 
     @Override
     public String toString() {
         return "ShareholderMapping{" +
-            "id=" + getId() +
             ", miraklShopId='" + getMiraklShopId() + "'" +
             ", miraklUboNumber=" + getMiraklUboNumber() +
             ", adyenShareholderCode='" + getAdyenShareholderCode() + "'" +
