e421c1736997845d1915b8b52b0335611ae46434
Aleffio <gigendh@gmail.com>
2018-03-16 13:31:03 +0100

    ADY-15(comments fixes) added constant for adyen-bankproof, added checktest on documentdelta, removed unused miraklshop

--- src/main/java/com/adyen/mirakl/config/Constants.java
+++ src/main/java/com/adyen/mirakl/config/Constants.java
@@ -11,7 +11,9 @@ public final class Constants {
     public static final String SYSTEM_ACCOUNT = "system";
     public static final String ANONYMOUS_USER = "anonymoususer";
     public static final String DEFAULT_LANGUAGE = "en";
-    
+
+    public static final String BANKPROOF = "adyen-bankproof";
+
     private Constants() {
     }
 }
