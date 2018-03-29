40d503a0fae2fe5f62994585dcaa345367075b4a
amrik.samra <amrik.samra@e2x.co.uk>
2018-03-29 15:17:55 +0100

    [AS] ADY-70 netherlands stripping address test

--- src/test/java/com/adyen/mirakl/cucumber/stepdefs/helpers/stepshelper/AssertionHelper.java
+++ src/test/java/com/adyen/mirakl/cucumber/stepdefs/helpers/stepshelper/AssertionHelper.java
@@ -1,10 +1,5 @@
 package com.adyen.mirakl.cucumber.stepdefs.helpers.stepshelper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.springframework.stereotype.Service;
 import com.adyen.model.marketpay.GetAccountHolderResponse;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -15,6 +10,12 @@ import com.mirakl.client.mmp.domain.shop.MiraklShop;
 import com.mirakl.client.mmp.domain.shop.bank.MiraklIbanBankAccountInformation;
 import com.mirakl.client.mmp.domain.shop.bank.MiraklPaymentInformation;
 import net.minidev.json.JSONArray;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class AssertionHelper {
@@ -22,19 +23,6 @@ public class AssertionHelper {
     private static final Map<String, String> CIVILITY_TO_GENDER = ImmutableMap.<String, String>builder().put("Mr", "MALE").put("Mrs", "FEMALE").put("Miss", "FEMALE").build();
 
     private DocumentContext parsedBankAccountDetail;
-
-    public ImmutableList.Builder<String> adyenAccountDataBuilder(DocumentContext notificationResponse) {
-        ImmutableList.Builder<String> adyenShopData = new ImmutableList.Builder<>();
-        adyenShopData.add(notificationResponse.read("content.accountHolderCode").toString());
-        adyenShopData.add(notificationResponse.read("content.accountHolderDetails.individualDetails.name.firstName").toString());
-        adyenShopData.add(notificationResponse.read("content.accountHolderDetails.individualDetails.name.lastName").toString());
-        adyenShopData.add(notificationResponse.read("content.accountHolderDetails.individualDetails.name.gender").toString());
-        adyenShopData.add(notificationResponse.read("content.accountHolderDetails.email").toString());
-        adyenShopData.add(notificationResponse.read("content.legalEntity").toString());
-        adyenShopData.add(notificationResponse.read("content.accountHolderDetails.address.city").toString());
-        adyenShopData.add(notificationResponse.read("content.accountHolderDetails.address.postalCode").toString());
-        return adyenShopData;
-    }
 
     public ImmutableList.Builder<String> adyenIndividualAccountDataBuilder(GetAccountHolderResponse getAccountHolderResponse) {
         ImmutableList.Builder<String> adyenShopData = new ImmutableList.Builder<>();
@@ -46,11 +34,10 @@ public class AssertionHelper {
         adyenShopData.add(getAccountHolderResponse.getLegalEntity().toString());
         adyenShopData.add(getAccountHolderResponse.getAccountHolderDetails().getAddress().getCity());
         adyenShopData.add(getAccountHolderResponse.getAccountHolderDetails().getAddress().getPostalCode());
-
         return adyenShopData;
     }
 
-    public ImmutableList.Builder<String> adyenShareHolderAccountDataBuilder(DocumentContext notificationResponse) {
+    public ImmutableList.Builder<String> adyenShareHolderAccountDataBuilder(MiraklShop shop, DocumentContext notificationResponse) {
         ImmutableList.Builder<String> adyenShopData = new ImmutableList.Builder<>();
 
         JSONArray uboArray = notificationResponse.read("content.accountHolderDetails.businessDetails.shareholders[*]");
@@ -60,7 +47,20 @@ public class AssertionHelper {
             adyenShopData.add(JsonPath.parse(ubo).read("ShareholderContact.name.lastName").toString());
             adyenShopData.add(JsonPath.parse(ubo).read("ShareholderContact.name.gender").toString());
             adyenShopData.add(JsonPath.parse(ubo).read("ShareholderContact.email").toString());
-
+            Object shareholderContact = JsonPath.parse(ubo).read("ShareholderContact");
+            if (shareholderContact.toString().contains("address")) {
+                adyenShopData.add(JsonPath.parse(ubo).read("ShareholderContact.address.city").toString());
+                adyenShopData.add(JsonPath.parse(ubo).read("ShareholderContact.address.country").toString());
+                adyenShopData.add(JsonPath.parse(ubo).read("ShareholderContact.address.postalCode").toString());
+                if (shop.getContactInformation().getCountry().equals("NLD")) {
+                    adyenShopData.add(
+                        JsonPath.parse(ubo).read("ShareholderContact.address.street").toString() + " " +
+                            JsonPath.parse(ubo).read("ShareholderContact.address.houseNumberOrName").toString());
+                } else {
+                    adyenShopData.add(JsonPath.parse(ubo).read("ShareholderContact.address.street").toString());
+                    adyenShopData.add(JsonPath.parse(ubo).read("ShareholderContact.address.houseNumberOrName").toString());
+                }
+            }
         }
         return adyenShopData;
     }
@@ -91,7 +91,6 @@ public class AssertionHelper {
 
     public ImmutableList.Builder<String> miraklShopShareHolderDataBuilder(MiraklShop miraklShop, List<Map<String, String>> rows) {
         ImmutableList.Builder<String> miraklShopData = new ImmutableList.Builder<>();
-
         List<String> shopAdditionalFields = new ArrayList<>();
         int maxUbos = Integer.parseInt(rows.get(0).get("maxUbos"));
         for (int i = 1; i < maxUbos; i++) {
@@ -99,20 +98,31 @@ public class AssertionHelper {
             shopAdditionalFields.add("adyen-ubo" + i + "-lastname");
             shopAdditionalFields.add("adyen-ubo" + i + "-civility");
             shopAdditionalFields.add("adyen-ubo" + i + "-email");
+            shopAdditionalFields.add("adyen-ubo" + i + "-city");
+            shopAdditionalFields.add("adyen-ubo" + i + "-country");
+            if (miraklShop.getContactInformation().getCountry().equals("NLD")) {
+                shopAdditionalFields.add("adyen-ubo" + i + "-streetname");
+            } else {
+                shopAdditionalFields.add("adyen-ubo" + i + "-housenumber");
+                shopAdditionalFields.add("adyen-ubo" + i + "-streetname");
+            }
+            shopAdditionalFields.add("adyen-ubo" + i + "-zip");
 
             for (String field : shopAdditionalFields) {
                 String fieldValue = miraklShop.getAdditionalFieldValues()
-                                              .stream()
-                                              .filter(MiraklAdditionalFieldValue.MiraklAbstractAdditionalFieldWithSingleValue.class::isInstance)
-                                              .map(MiraklAdditionalFieldValue.MiraklAbstractAdditionalFieldWithSingleValue.class::cast)
-                                              .filter(x -> field.equals(x.getCode()))
-                                              .map(MiraklAdditionalFieldValue.MiraklAbstractAdditionalFieldWithSingleValue::getValue)
-                                              .findAny()
-                                              .orElse("");
+                    .stream()
+                    .filter(MiraklAdditionalFieldValue.MiraklAbstractAdditionalFieldWithSingleValue.class::isInstance)
+                    .map(MiraklAdditionalFieldValue.MiraklAbstractAdditionalFieldWithSingleValue.class::cast)
+                    .filter(x -> field.equals(x.getCode()))
+                    .map(MiraklAdditionalFieldValue.MiraklAbstractAdditionalFieldWithSingleValue::getValue)
+                    .findAny()
+                    .orElse("");
                 if (field.contains("civility")) {
                     miraklShopData.add(CIVILITY_TO_GENDER.get(fieldValue));
                 } else {
-                    miraklShopData.add(fieldValue);
+                    if (!fieldValue.isEmpty()){
+                        miraklShopData.add(fieldValue);
+                    }
                 }
             }
         }
@@ -140,13 +150,13 @@ public class AssertionHelper {
         miraklShopData.add(CIVILITY_TO_GENDER.get(miraklShop.getContactInformation().getCivility()));
         miraklShopData.add(email);
         String element = miraklShop.getAdditionalFieldValues()
-                                   .stream()
-                                   .filter(MiraklAdditionalFieldValue.MiraklValueListAdditionalFieldValue.class::isInstance)
-                                   .map(MiraklAdditionalFieldValue.MiraklValueListAdditionalFieldValue.class::cast)
-                                   .filter(x -> "adyen-legal-entity-type".equals(x.getCode()))
-                                   .map(MiraklAdditionalFieldValue.MiraklValueListAdditionalFieldValue::getValue)
-                                   .findAny()
-                                   .orElse("");
+            .stream()
+            .filter(MiraklAdditionalFieldValue.MiraklValueListAdditionalFieldValue.class::isInstance)
+            .map(MiraklAdditionalFieldValue.MiraklValueListAdditionalFieldValue.class::cast)
+            .filter(x -> "adyen-legal-entity-type".equals(x.getCode()))
+            .map(MiraklAdditionalFieldValue.MiraklValueListAdditionalFieldValue::getValue)
+            .findAny()
+            .orElse("");
         miraklShopData.add(element);
         miraklShopData.add(miraklShop.getContactInformation().getCity());
         miraklShopData.add(miraklShop.getContactInformation().getZipCode());
