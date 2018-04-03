da32705a9da31598396153f87ad42b081b889d74
Rik ter Beek <rik.terbeek@adyen.com>
2018-04-03 19:15:04 +0200

    initail commit for ADY-24

--- src/test/java/com/adyen/mirakl/service/MailTemplateServiceTest.java
+++ src/test/java/com/adyen/mirakl/service/MailTemplateServiceTest.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import com.adyen.mirakl.service.MailTemplateService;
+import com.adyen.mirakl.config.MiraklOperatorConfiguration;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,7 +19,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import com.adyen.mirakl.AdyenMiraklConnectorApp;
-import com.adyen.mirakl.service.MailService;
+import com.adyen.model.marketpay.Message;
 import com.google.common.io.Resources;
 import com.mirakl.client.core.internal.mapper.CustomObjectMapper;
 import com.mirakl.client.mmp.domain.shop.MiraklContactInformation;
@@ -49,12 +49,15 @@ public class MailTemplateServiceTest {
     @Autowired
     private MessageSource messageSource;
 
+    @Autowired
+    private MiraklOperatorConfiguration miraklOperatorConfiguration;
+
     @Captor
     private ArgumentCaptor<String> contentCaptor;
 
     @Before
     public void setup() {
-        mailTemplateService = new MailTemplateService(jHipsterProperties, mailServiceMock, templateEngine, messageSource);
+        mailTemplateService = new MailTemplateService(jHipsterProperties, mailServiceMock, templateEngine, messageSource, miraklOperatorConfiguration);
     }
 
     @Test
@@ -73,7 +76,7 @@ public class MailTemplateServiceTest {
     }
 
     @Test
-    public void sendAccountHolderValidationEmail() {
+    public void testSendOperatorEmailPayoutFailure() {
         final MiraklShop miraklShop = new MiraklShop();
         final MiraklContactInformation miraklContactInformation = new MiraklContactInformation();
         miraklShop.setContactInformation(miraklContactInformation);
@@ -96,4 +99,31 @@ public class MailTemplateServiceTest {
         errors.forEach((e) -> Assertions.assertThat(content).contains(e));
     }
 
+    @Test
+    public void sendAccountHolderPayoutFailedEmail() {
+
+        final MiraklShop miraklShop = new MiraklShop();
+        final MiraklContactInformation miraklContactInformation = new MiraklContactInformation();
+        miraklShop.setContactInformation(miraklContactInformation);
+
+        miraklContactInformation.setCivility("Mr");
+        miraklContactInformation.setFirstname("John");
+        miraklContactInformation.setLastname("Doe");
+        miraklContactInformation.setEmail("adyen-mirakl-cb966314-55c3-40e6-91f7-db6d8f0be825@mailinator.com");
+
+        doNothing().when(mailServiceMock).sendEmail(isA(String.class), isA(String.class), contentCaptor.capture(), anyBoolean(), anyBoolean());
+
+        final String code = "10_063";
+        final String text = "Payout is not allowed because the account does not have the payout state.";
+
+        Message message = new Message();
+        message.setCode(code);
+        message.setText(text);
+        mailTemplateService.sendOperatorEmailPayoutFailure(miraklShop, message);
+
+        final String content = contentCaptor.getValue();
+        // verify that all the payout code and text are there
+        Assertions.assertThat(content).contains(code);
+        Assertions.assertThat(content).contains(text);
+    }
 }
